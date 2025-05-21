0009bbb9bfd354355c702dbf5774428f4a21cc89
Sean McKenna <seanmck@microsoft.com>
2025-05-20 19:05:35 -0700

    Adding types package

---
+++ packages/types/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.POSITIONS = void 0;
+exports.POSITIONS = [
+    'P', 'C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF'
+];
